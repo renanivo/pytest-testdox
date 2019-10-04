51aa77a3b9de0200bc26f15a4b8aa5d877df7f58
Renan Ivo <renan.martins@luizalabs.com>
2019-10-04 11:13:20 -0300

    Bump version: 1.1.1 → 1.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-testdox',
-    version='1.1.1',
+    version='1.2.0',
     description='A testdox format reporter for pytest',
     long_description=read('README.rst'),
     author='Renan Ivo',
