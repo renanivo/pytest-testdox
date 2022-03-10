c15ba7e44a8d676078148ee21a4722fe70c85b8d
Senopiece <shichiromail@gmail.com>
2022-03-10 21:30:14 +0300

    apply back format

--- pytest_testdox/plugin.py
+++ pytest_testdox/plugin.py
@@ -112,7 +112,7 @@ class TestdoxTerminalReporter(TerminalReporter):
 
     def pytest_runtest_logreport(self, report):
         self._register_stats(report)
-        
+
         if report.skipped and report.when == 'teardown':
             return
         if report.when != 'call' and not report.skipped:
